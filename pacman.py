f7de41ab9be55609392df8aa0a61d7ad91453b57
Peh099 <70337717+Peh099@users.noreply.github.com>
2023-04-29 12:38:36 -0300

    Adicionando pacman e fantasmas
    Co-authored-by: Hian <hianpraxedes4@gmail.com>

--- pacman.py
+++ pacman.py
@@ -23,6 +23,38 @@ tela = pygame.display.set_mode(tamanho)
 # Define a fonte
 fonte = pygame.font.SysFont(None, 25)
 
+#Define a classe do Fantasma
+class Fantasma:
+    def __init__(self, x, y, elemento_size):
+        self.elemento_size = elemento_size
+        self.color = VERMELHO
+        self.set_posicao((x, y))
+
+    def set_posicao(self, posicao):
+        if posicao in grafo:
+            self.posicao = posicao
+
+    def draw(self):
+        x, y = self.posicao
+        pygame.draw.circle(tela, self.color, [y*self.elemento_size+self.elemento_size//2, x*self.elemento_size+self.elemento_size//2], self.elemento_size//2)
+
+# Define a classe Pacman
+class Pacman:
+    def __init__(self, x, y, elemento_size):
+        self.elemento_size = elemento_size
+        self.color = AMARELO
+        self.set_posicao((x, y))
+        self.pontos = 0
+        self.visitados = set()
+
+    def set_posicao(self, posicao):
+        if posicao in grafo:
+            self.posicao = posicao
+
+    def draw(self):
+        x, y = self.posicao
+        pygame.draw.circle(tela, self.color, [y*self.elemento_size+self.elemento_size//2, x*self.elemento_size+self.elemento_size//2], self.elemento_size//2)
+
 def get_vizinhos(i, j):
     vizinhos = []
     if i > 0 and mapa[i-1][j] != "#":
@@ -49,6 +81,15 @@ def desenha_mapa(mapa, tamanho_elemento):
         for j, coluna in enumerate(linha):
             if coluna == "#":
                 pygame.draw.rect(tela, AZUL, [j*tamanho_elemento, i*tamanho_elemento, tamanho_elemento, tamanho_elemento])
+
+
+# Cria o Pac-Man
+pacman = Pacman(25, 13, 20)      
+
+# Cria os fantasmas
+fantasma1 = Fantasma(10, 12, 20)
+fantasma2 = Fantasma(10, 14, 20)
+fantasma3 = Fantasma(10, 13, 20)
                 
 # Loop principal do jogo
 checar = False
@@ -61,9 +102,13 @@ while not checar:
 
     # Limpa a tela
     tela.fill(PRETO)
-    
-    # Atualiza a tela
-    pygame.display.update()
+
+    # Desenha o Pac-Man
+    pacman.draw()
+    # Desenha os fantasmas
+    fantasma1.draw()
+    fantasma2.draw()
+    fantasma3.draw()
 
     # Desenha o mapa
     desenha_mapa(mapa, 20)
